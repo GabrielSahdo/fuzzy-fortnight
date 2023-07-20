e143bb1ebb69efa4c9ed182d2f5d6a62cb0a4264
Gabriel Sahdo <sahdo@motorola.com>
2023-07-19 20:23:37 -0400

    setting up lint/formater/husky

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia';
 
-const app = new Elysia()
-  .get("/ping", () => "pong")
-  .listen(3000);
+const a ="a";
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+const app = new Elysia().get('/ping', () => 'pong').listen(3000);
+
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
